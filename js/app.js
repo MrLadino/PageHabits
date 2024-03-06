2bb803422a25b94af1a099df892de1ff8f983483
unknown <SenaTarde@ARMAPRRQDFSD205.FormacionSena.RED>
2024-03-06 16:14:22 -0500

    other chages

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-// Obtén todos los elementos h1 y h2
-let headings = document.querySelectorAll('h1, h2');
+// Obtén todos los elementos h1, h2 y a
+let elements = document.querySelectorAll('h1, h2, a');
 
 // Itera sobre cada elemento y agrega un event listener para el mouseover
-headings.forEach((heading) => {
-    heading.addEventListener('mouseover', () => {
-        heading.classList.add('move'); // Agrega la clase 'move' al elemento
+elements.forEach((element) => {
+    element.addEventListener('mouseover', () => {
+        element.classList.add('move'); // Agrega la clase 'move' al elemento
     });
 
     // Agrega un event listener para el mouseout para quitar la clase 'move'
-    heading.addEventListener('mouseout', () => {
-        heading.classList.remove('move'); // Quita la clase 'move' al elemento
+    element.addEventListener('mouseout', () => {
+        element.classList.remove('move'); // Quita la clase 'move' al elemento
     });
 });
+
